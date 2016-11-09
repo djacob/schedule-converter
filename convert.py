2f1e3493e0c0137d5824bce6e82ff889885fb988
Jacob Duell <jacob.duell@altamiracorp.com>
2016-11-09 14:49:55 -0500

    Print out dates and schedules to console

--- convert.py
+++ convert.py
@@ -2,40 +2,49 @@
 
 import sys
 import xlrd
-import unicodecsv as csv
+from datetime import date as datetime
 
-def xls_to_csv(xls_path, csv_path):
+def xls_to_list(xls_path):
     """
     Converts the xls file at the given path and writes out a csv file
     :param xls_path the input xls file path
-    :param csv_path the output csv file path
     """
     workbook = xlrd.open_workbook(xls_path)
-    print('sheets {0}'.format(workbook.sheets()))
     worksheet = workbook.sheet_by_index(1)
-    csvfile = open(csv_path, 'wb')
-    wr = csv.writer(csvfile)
-
+    sheet = []
     for rownum in xrange(worksheet.nrows):
         row = []
         for cell in worksheet.row_values(rownum):
             if type(cell) == type(u''):
                 row.append(cell.encode('utf-8'))
-            elif type(cell) == type(0.1) and cell > 0 and cell < 1:
-                time = xlrd.xldate_as_tuple(cell, datemode=1)
-                row.append("{0}:{1}".format(time[3],time[4]))
+            elif type(cell) == type(0.1) and cell > 0:
+                date = xlrd.xldate_as_tuple(cell, datemode=1)
+                if date[0] == 0:
+                    row.append("{0}:{1}".format(date[3], date[4]))
+                else:
+                    row.append("{0}-{1}-{2}".format(date[1], date[2] - 1, datetime.today().year))
             else:
                 row.append(cell)
-        wr.writerow(row)
+        sheet.append(row)
 
-    csvfile.close()
+    return sheet
 
 if __name__ == "__main__":
     xls_path = sys.argv[1]
-    csv_path = sys.argv[2]
+    sheet = xls_to_list(xls_path=xls_path)
 
-    if xls_path and csv_path:
-        xls_to_csv(xls_path, csv_path)
-    else:
-        print('usage {0} xls_path csv_path'.format(sys.argv[0]))
-
+    dates = []
+    for i, row in enumerate(sheet):
+        if row[3] == 'Sunday':
+            dates = sheet[i + 1]
+            print('found dates {0}'.format(dates))
+        if row[1] == 'Meg':
+            print('found meg')
+            time_in = sheet[i]
+            time_out = sheet[i + 1]
+            prev_time = None
+            for i, date in enumerate(dates):
+                shift = 'first' if date else ('second' if prev_time else None)
+                if time_in != '' and shift:
+                    print("working on {0} {1} from {2} to {3}".format(date, shift, time_in[i], time_out[i]))
+                prev_time = date if date != '' else None
